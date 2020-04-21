a93b072bf8cc86890fdd8479481a67f7b4c97856
Visa <visalakshi.cse@gmail.com>
2020-04-21 02:18:10 -0500

    User profile added

---
+++ src/userprofiles.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { Link, withRouter } from 'react-router-dom'
+
+
+
+
+
+export default class Userprofiles extends React.Component{
+    constructor(props){
+        super(props)
+    }
+render(){
+
+    return(
+
+<div>
+
+</div>
+
+
+
+
+    );
+}
+    }
