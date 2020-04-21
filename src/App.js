f1a70285f58e55e5357823d80973b6a78929722f
Visa <visalakshi.cse@gmail.com>
2020-04-20 20:21:48 -0500

    Login file called

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Register from './Login/register'
+import Login from './Login/login'
 
 function App() {
   return (
     <div className="App">
-      
+      <Login />
     </div>
   );
 }
